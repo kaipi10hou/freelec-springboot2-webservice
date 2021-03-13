440778271b37e4e3bc4c79155599ccf33d4a2af8
CherryOnTop <kaipi10hou@gmail.com>
2021-03-13 21:48:06 +0900

    posts-update

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -28,7 +28,7 @@ public class IndexController {
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model){
         PostsResponseDto dto = postsService.findById(id);
-
+        model.addAttribute("post",dto);
         return "posts-update";
     }
 
