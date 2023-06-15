46b23ecb882eb522eb87e98f1df79247404044e3
kaipi10hou <kaipi10hou@gmail.com>
2023-06-15 23:06:16 +0900

    JPA Entity POST test code

---
+++ src/test/java/com/springboot/book/startspringbootwebservice/HelloControllerTest.java
@@ -0,0 +1,45 @@
+package com.springboot.book.startspringbootwebservice;
+
+import com.springboot.book.startspringbootwebservice.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class) //SpringRunner라는 스프링 실행자를 사욯애서 실행 => 스프링부트테스트와 JUnit 사이 연결자역할
+@WebMvcTest(controllers = HelloController.class) //선언할 경우 @Controller, @ContollerAdvice 사용가능
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_return() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
+}
